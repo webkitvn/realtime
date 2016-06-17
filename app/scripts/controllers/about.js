b76ae960dfa8656e733762e112b1310960cb8261
Cuong Pham <Cuong Pham>
2016-06-17 15:17:49 +0700

    add yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name realtimeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the realtimeApp
+ */
+angular.module('realtimeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name realtimeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the realtimeApp
+ */
+angular.module('realtimeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
